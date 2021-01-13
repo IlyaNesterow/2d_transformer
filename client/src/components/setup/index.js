aa17efcffe04893fd035b5d840f09bb1f49dddda
IlyaNesterow <j.nesterov1709@gmail.com>
2021-01-14 01:31:46 +0200

    Routes for static serving are fixed

--- client/src/components/setup/index.js
+++ client/src/components/setup/index.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import SetupForm from './SetupForm'
 
 
 const Setup = () => {
+  const [ image, setImage ] = useState(null)
+
   const handleSetupSubmission = (setup) => {
-    console.log(setup)
+    fetch('/plot', {
+      method: 'PATCH', 
+      body: JSON.stringify(setup),
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+      .then(res => res.blob())
+      .then(res => setImage(URL.createObjectURL(res)))
   }
 
   return(
     <SetupWrapper>
       <SetupForm handleSubmission={ handleSetupSubmission }/>
+      { image !== null && 
+        <img src={ image } alt="plot"/>
+      }
     </SetupWrapper>
   )
 }
