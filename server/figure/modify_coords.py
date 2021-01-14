ee9ee81200920cd0d607920eeb51858374aa7fee
IlyaNesterow <j.nesterov1709@gmail.com>
2021-01-14 21:16:48 +0200

    rotate -> angle

--- server/figure/modify_coords.py
+++ server/figure/modify_coords.py
@@ -7,6 +7,6 @@ from server.utils.modify_cords import modify
 def modify_coords(body: dict, x: list, y: list) -> list and list:
     x, y = modify(body.get('move', None), x, y, move)
     x, y = modify(body.get('scale', None), x, y, scale)
-    x, y = modify(body.get('rotate', None), x, y, rotate)
+    x, y = modify(body.get('angle', None), x, y, rotate)
 
     return x, y
